2319aec06fc9a658ab854a955f312f1c8f86b6a1
lradovic <lukaradovic96@gmail.com>
2020-02-08 21:50:51 +0100

    vreme REcenzije

--- demo/demo/src/main/java/root/demo/repository/UserRepository.java
+++ demo/demo/src/main/java/root/demo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package root.demo.repository;
 
+import java.util.List;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
 	
 	public User findByEmail(String email);
 	public User findByUsername(String username);
+	public List<User> findByRole(String role);
 
 
 }
